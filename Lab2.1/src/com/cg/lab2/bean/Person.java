47e0f69519df723553fb838a3e3767b3352022f5
Neha Singh <47055337+NehaSingh2608@users.noreply.github.com>
2019-02-13 14:08:40 +0530

    Lab2.1

---
+++ Lab2.1/src/com/cg/lab2/bean/Person.java
@@ -0,0 +1,15 @@
+package com.cg.lab2.bean;
+
+public class Person {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("First Name:" +"Divya");
+		System.out.println("Last Name:" +"Bharthi");
+		System.out.println("Gender:"+"F");
+		System.out.println("Age:"+ 20);
+		System.out.println("Weight:"+ 85.55);
+
+	}
+
+}
